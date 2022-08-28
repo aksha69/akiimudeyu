98bd0af120045b7f9501ea87014e711f5dcacf68
akshay mude <akshay mude@LAPTOP-8ROU7R0R>
2022-08-28 12:55:58 +0530

    first commit

---
+++ src/akiimudeyu12/eygug.java
@@ -0,0 +1,21 @@
+package akiimudeyu12;
+
+public class eygug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my new java program");
+		System.out.println("my java");
+		System.out.println("whats up bro");
+		char a;
+		System.out.println(10+12+"who");
+		
+		System.out.println("who"+10+12);
+		
+        System.out.println(10*12+"who");
+		
+		System.out.println("who"+10*12);
+	}
+
+}
+
